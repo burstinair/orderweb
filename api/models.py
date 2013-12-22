033794f96cf7c178f1d8229ebbe8ea5eb8c4b268
Burst <burstinair@gmail.com>
2013-12-23 00:23:24 +0800

    finish submit

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Dish(models.Model):
+    shop = models.ForeignKey('Shop')
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    device_id = models.CharField(max_length=50)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order')
+    dish = models.ForeignKey('Dish')
+    count = models.IntegerField()
--- api/views.py
+++ api/views.py
@@ -1,28 +1,49 @@
 # coding=utf8
 from django.http import HttpResponse
 import json
+from models import *
+
+
+# 默认全部为同一Shop
+shop = Shop(1, name="默认店")
+shop.save()
 
 
 def resolve(request):
     print len(request.body)
 
-    # test: 将图片写入文件
+    # FixMe test: 将图片写入文件
     f = open('tmp/test.jpg', 'wb')
     f.write(request.body)
     f.close()
 
-    # 返回解析结果
-    result = {'status': 'success', 'result': [
-        {'id': 1, 'name': '麻婆豆腐', 'price': 34.5},
-        {'id': 2, 'name': '红烧肉', 'price': 23}
-    ]}
+    # TODO 解析结果
+    resolveResult = [
+        {'name': '麻婆豆腐', 'price': 34.5},
+        {'name': '红烧肉', 'price': 23}
+    ]
+
+    for dishResult in resolveResult:
+        try:
+            dish = Dish.objects.get(shop=shop, name=dishResult['name'])
+        except Exception:
+            dish = Dish(shop=shop, name=dishResult['name'], price=dishResult['price'])
+        dish.save()
+        dishResult['id'] = dish.pk
+
+    # 返回结果
+    result = {'status': 'success', 'result': resolveResult}
     return HttpResponse(json.dumps(result))
 
 
 def submit(request):
     print request.body
 
-    # TODO 保存点菜结果
+    params = json.loads(request.body, "utf-8")
+    order = Order(device_id=params['deviceId'])
+    order.save()
+    for orderItemRaw in params['orders']:
+        orderItem = OrderItem(order=order, dish=Dish(orderItemRaw['id']), count=orderItemRaw['count'])
+        orderItem.save()
 
     return HttpResponse('{"status": "success"}')
- 
